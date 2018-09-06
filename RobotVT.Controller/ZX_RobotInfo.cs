2ea868e523a04dfebff1569b433329e4e2cbb079
kodazzgm <kodazzgm@163.com>
2018-09-06 17:52:01 +0800

    123

---
+++ RobotVT.Controller/ZX_RobotInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RobotVT.Controller
+{
+    public partial class ZX_RobotInfo : UserControl
+    {
+        public ZX_RobotInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void ZX_RobotInfo_Load(object sender, EventArgs e)
+        {
+            this.topMain.Style.BackgroundImage = RobotVT.Resources.Properties.Resources.top_img;
+            this.signal.Style.BackgroundImage = RobotVT.Resources.Properties.Resources.signal_5;
+            this.power1.Style.BackgroundImage = RobotVT.Resources.Properties.Resources.power1_3;
+            this.power2.Style.BackgroundImage = RobotVT.Resources.Properties.Resources.power2_2;
+            this.robotPower.Style.BackgroundImage = RobotVT.Resources.Properties.Resources.power3_3;
+            this.lamp.Style.BackgroundImage = RobotVT.Resources.Properties.Resources.lamp_0;
+        }
+
+
+    }
+}
