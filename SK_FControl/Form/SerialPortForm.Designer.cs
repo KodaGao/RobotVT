3539fbff1ad5f8c897b2915c82ce2344f2a478f1
kodazzgm <kodazzgm@163.com>
2018-11-15 17:30:55 +0800

    增加串口配置界面

---
+++ SK_FControl/Form/SerialPortForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace SK_FControl
+{
+    partial class SerialPortForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reflectionImage = new DevComponents.DotNetBar.Controls.ReflectionImage();
+            this.SuspendLayout();
+            // 
+            // reflectionImage
+            // 
+            // 
+            // 
+            // 
+            this.reflectionImage.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.reflectionImage.BackgroundStyle.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.reflectionImage.Image = global::SK_FControl.Properties.Resources.serialport02;
+            this.reflectionImage.Location = new System.Drawing.Point(12, 45);
+            this.reflectionImage.Name = "reflectionImage";
+            this.reflectionImage.Size = new System.Drawing.Size(152, 274);
+            this.reflectionImage.TabIndex = 8;
+            // 
+            // SerialPortForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 409);
+            this.Controls.Add(this.reflectionImage);
+            this.DoubleBuffered = true;
+            this.Name = "SerialPortForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "SerialPortForm";
+            this.Load += new System.EventHandler(this.SerialPortForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.Controls.ReflectionImage reflectionImage;
+    }
+}
---
+++ SK_FControl/Form/SerialPortForm.cs
@@ -0,0 +1,26 @@
+﻿using DevComponents.DotNetBar.Metro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SK_FControl
+{
+    public partial class SerialPortForm : MetroForm
+    {
+        public SerialPortForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SerialPortForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
