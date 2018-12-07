674a3232410dbcf6b88c13f154f4a2bb816e3ddb
kodazzgm <kodazzgm@163.com>
2018-12-07 19:22:08 +0800

    1

--- RobotVT.Resources/Properties/Resources.Designer.cs
+++ RobotVT.Resources/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace RobotVT.Resources.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        public static System.Drawing.Bitmap CloudControl {
+            get {
+                object obj = ResourceManager.GetObject("CloudControl", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         public static System.Drawing.Bitmap comparebg {
             get {
                 object obj = ResourceManager.GetObject("comparebg", resourceCulture);
